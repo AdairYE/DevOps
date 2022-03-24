d870070af7d723bff6e82abcdc113131d2e5f812
Zejun YE <yezejun@cndatacom.com>
2022-03-24 18:09:38 +0800

    【update】 add register

--- Auth/froms/froms.py
+++ Auth/froms/froms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.forms import fields
 from django.contrib.auth.models import User
 from django.forms import widgets as wid
+from django.core.exceptions import ValidationError
 
 
 class userLogin(forms.ModelForm):
@@ -12,3 +13,25 @@ class userLogin(forms.ModelForm):
             "username": wid.TextInput(attrs={'class': 'form-control'}),
             "password": wid.PasswordInput(attrs={'class': 'form-control'}),
         }
+
+class userRegister(forms.ModelForm):
+    againPassword = forms.CharField(
+        label="确认密码",
+        widget=forms.PasswordInput(),
+        min_length=8,
+        max_length=32,
+        error_messages={
+            "min_length":"密码长度不能小于8个字符",
+            "max_length":"密码长度不能大于32个字符"
+        }
+    )
+
+    class Meta:
+        model = User
+        fields = ["username","email","password","againPassword"]
+
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        for field in self.fields.values():
+            field.widget.attrs["class"] = "form-control"
+            field.widget.attrs["placeholder"] = "请输入{}".format(field.label)
--- Auth/views.py
+++ Auth/views.py
@@ -3,23 +3,46 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, JsonResponse
 
-from Auth.froms.froms import userLogin
+from Auth.froms.froms import userLogin,userRegister
 
 
 def indexHome(request):
     executeInfo = {"status": "true", "msg": None}
     if request.method == "GET":
         userLoginInfo = userLogin()
-        return render(request, "auth/base.html", {"userLoginInfo": userLoginInfo})
+        userRegisterInfo = userRegister()
+        return render(request, "auth/base.html", {"userLoginInfo": userLoginInfo,"userRegisterInfo":userRegisterInfo})
     elif request.method == "POST":
-        # 登录
-        username = request.POST.get("username", None)
-        password = request.POST.get("password", None)
-        loginCheck = authenticate(username=username, password=password)
-        if loginCheck:
-            login(request, user=loginCheck)
+        if len(request.POST) > 2:
+            userInfo = userRegister(request.POST)
+            if userInfo.is_valid():
+                userInfo.save()
+            else:
+                executeInfo["status"] = "false"
+                executeInfo["msg"] = userInfo.errors
             return JsonResponse(executeInfo)
         else:
-            executeInfo["status"] = "false"
-            executeInfo["msg"] = "登录失败，请重新登录!"
+            userInfo = userLogin(request.POST)
+            if userInfo.is_valid():
+                validInfo = userInfo.cleaned_data
+                username = validInfo["username"]
+                password =validInfo["password"]
+                loginCheck = authenticate(username=username, password=password)
+                if loginCheck:
+                    login(request, user=loginCheck)
+                else:
+                    executeInfo["status"] = "false"
+                    executeInfo["msg"] = "登录失败，请重新登录!"
             return JsonResponse(executeInfo)
+
+        # # 登录
+        # username = request.POST.get("username", None)
+        # password = request.POST.get("password", None)
+        # loginCheck = authenticate(username=username, password=password)
+        # if loginCheck:
+        #     login(request, user=loginCheck)
+        #     return JsonResponse(executeInfo)
+        # else:
+        #     executeInfo["status"] = "false"
+        #     executeInfo["msg"] = "登录失败，请重新登录!"
+        #     return JsonResponse(executeInfo)
