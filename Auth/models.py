07f57dac43a1b7f109e4c8b96c07096b9c8b869d
Zejun YE <yezejun@cndatacom.com>
2022-03-31 18:08:08 +0800

    【update】 增加动态导航

--- Auth/models.py
+++ Auth/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
+
 # Create your models here.
+
+# 菜单
+class Menus(models.Model):
+    name = models.CharField(
+        max_length=32,
+        verbose_name="菜单")
+    show = models.BooleanField(
+        verbose_name="是否显示",
+        default=False,
+        help_text="菜单是否显示，默认添加不显示")
+    url = models.CharField(
+        max_length=300,
+        verbose_name="菜单URL地址",
+        null=True,
+        blank=True,
+        default="javascript:void(0)",
+        help_text="为菜单设置url地址")
+    priority = models.IntegerField(
+        verbose_name="优先级",
+        null=True,
+        blank=True,
+        default=-1,
+        help_text="菜单显示的顺序，数字越大越靠前"
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "菜单"
+        verbose_name_plural = verbose_name
+
+# 父级菜单
+class parentMenus(models.Model):
+    parent = models.ForeignKey(
+        Menus, verbose_name="父级菜单",
+        null=True,
+        blank=True,
+        default=0,
+        on_delete=models.CASCADE,
+        help_text="添加子菜单时需要选择父级菜单")
+
+    def __str__(self):
+        return self.parent
+
+    class Meta:
+        verbose_name = "父级菜单"
+        verbose_name_plural = verbose_name
